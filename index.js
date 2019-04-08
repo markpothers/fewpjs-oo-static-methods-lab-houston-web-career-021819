d2fc4049f1a66c141a4b15635879dbd8f09b3dd2
Mark R Pothecary <markpothers@hotmail.com>
2019-04-08 17:29:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    let array = string.split(" ")
+    let newArray = []
+    array.forEach(function(word){
+      if (!(word == 'the' || word == 'a' || word == 'an' || word == 'but' || word == 'of' ||word == 'and'||word =='for'||word =='by'||word=='from'||word=='at')){
+        newArray.push(word[0].toUpperCase() + word.slice(1))
+      } else {
+        newArray.push(word)
+      }
+    })
+    let newString = newArray.join(" ")
+    return newString.charAt(0).toUpperCase() + newString.slice(1)
+  }
+
+
 }
